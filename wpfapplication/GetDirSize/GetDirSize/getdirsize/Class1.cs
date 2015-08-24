acd28e8107e4c311efdd5327a0b1af078fce2069
Daniel McCoy <danielmccoy85@gmail.com>
2015-08-24 12:09:11 -0700

    added DLL code

---
+++ wpfapplication/GetDirSize/GetDirSize/getdirsize/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDirSize
+{
+    public class DirSize
+    {
+        static string GetDirectorySize(string p)
+        {
+            try
+            {
+                // 1.
+                // Get array of all file names.
+                string[] a = Directory.GetFiles(p, "*.*", SearchOption.AllDirectories);
+                int i;
+                //for (i = 0; i < a.Length; i++)
+                //{ Console.WriteLine(a[i]); }
+
+
+                // 2.
+                // Calculate total bytes of all files in a loop.
+                long b = 0;
+                foreach (string name in a)
+                {
+                    // 3.
+                    // Use FileInfo to get length of each file.
+                    FileInfo info = new FileInfo(name);
+                    b += info.Length;
+                }
+                // 4.
+                // Return total size
+                return Convert.ToString(b);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                return "Access to file " + p + " was denied";
+
+            }
+        }
+    }
+}
---
+++ wpfapplication/GetDirSize/GetDirSize/getdirsize/DirSize.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDirSize
+{
+    public class DirSize
+    {
+       public static string GetDirectorySize(string p)
+        {
+            try
+            {
+                // Get array of all file names.
+                string[] a = Directory.GetFiles(p, "*.*", SearchOption.AllDirectories);
+                
+                
+                //list all of the files in the diectory. can be too expensive
+                //int i;
+                //for (i = 0; i < a.Length; i++)
+                //{ Console.WriteLine(a[i]); }
+
+                // Calculate total bytes of all files in a loop.
+                long b = 0;
+                foreach (string name in a)
+                {
+                    // Use FileInfo to get length of each file.
+                    FileInfo info = new FileInfo(name);
+                    b += info.Length;
+                }
+
+                // Return total size
+                return Convert.ToString(b);
+            }
+                //dont have permissions to the file
+            catch (UnauthorizedAccessException e)
+            {
+                return "Access to file " + p + " was denied";
+
+            }
+        }
+    }
+}
